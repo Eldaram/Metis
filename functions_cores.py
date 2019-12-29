3c755b3faae5b0e228b13264765742578433e4d3
Dragan Ribes <dragan.ribes@gmail.com>
2019-12-29 18:43:16 +0100

    Partial

--- functions_cores.py
+++ functions_cores.py
@@ -1,3 +1,2 @@
 def Test_token(secretToken):
  	return len(secretToken) == 59
- 	''' Think to upgrade this function... To avoid evry type of error.'''
--- launcher.py
+++ launcher.py
@@ -1,11 +1,11 @@
+#modules for Metis
+import data
 import functions_cores
-secret_token = ""
-while not functions_cores.Test_token(secret_token):
-	if not secret_token == "":
-		print("This is not a token. Please retry... \n" + str(len(secret_token)))
-	secret_token = input("Please enter your Secret Token here:")
-	print("\n")
 
-print("Your secret token is valid. Metis is launching... \n")
+#MAIN
+if functions_cores.Test_token(secret_token):
+	print("Your secret token is valid. Metis is launching... \n")
+	import Metis
 
-print("Sorry, Metis is not avalidable right now...")
+else :
+	print("This is not a token. Please retry... \n")
