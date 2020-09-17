0ccfe85fc1ab4f290941b9ba4006104b91560f16
Dragan Ribes <dragan.ribes@gmail.com>
2020-09-17 13:39:29 +0200

    in_list update : Ajout de la variable f

--- functions_cores.py
+++ functions_cores.py
@@ -14,9 +14,17 @@ def read_last():
 	file.close()
 	return int(content)
 
-def in_list(lis,arg):
+"""
+in_list permet de savoir si un arg se trouve dans une liste lis
+return -> bolean
+lis    <- list[a]
+arg    <- a
+
+f <= anonymus function
+"""
+def in_list(lis,arg, f=(lambda x : x)):
 	length = len(lis)
 	i = 0
-	while (i<length and lis[i] != arg):
+	while (i<length and f(lis[i]) != arg):
 		i += 1
 	return i<length
